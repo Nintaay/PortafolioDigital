cf849e87a651ffc0431f66b9812137895cfed5de
Nintaay <morenojoel699@gmail.com>
2023-12-05 14:17:19 -0600

    se agregaron comentarios para entenderlo mejor

--- js/questions.js
+++ js/questions.js
@@ -1,24 +1,24 @@
 (function(){
-     // Selecciona todos los elementos con la clase 'questions__title'
+     // Selecciona todos los elementos con la clase 'questions__title' y se convierte en array
     const titleQuestions = [...document.querySelectorAll('.questions__title')];
    
-// Itera sobre cada elemento seleccionado
+// Itera sobre cada elemento seleccionado, es decir que de las 3 yo solo quier la seleccionada
     titleQuestions.forEach(question =>{
          // Agrega un event listener para el evento 'click' a cada pregunta
         question.addEventListener('click', ()=>{
             let height = 0;
-            let answer = question.nextElementSibling;
-            let addPadding = question.parentElement.parentElement;
+            let answer = question.nextElementSibling;//este es el siguiente elemento osea el parrafo answer
+            let addPadding = question.parentElement.parentElement; //toma todo el elemento de la pregunta
  // Agrega o quita una clase para cambiar el estilo (puede ser para agregar padding)
             addPadding.classList.toggle('questions__padding--add');
-             // Rota un ícono de flecha (posiblemente para indicar si la pregunta está abierta o cerrada)
+             // rota la flecha dependiendo si tiene la clase 
             question.children[0].classList.toggle('questions__arrow--rotate');
   // Si la altura de la respuesta es 0, establece la altura como la altura total de la respuesta
-            if(answer.clientHeight === 0){
-                height = answer.scrollHeight;
+            if(answer.clientHeight === 0){ // si el elemento tiene un hight de o 
+                height = answer.scrollHeight;// a height se le agrega scrollheight que es el minimo para que se muestre
             }
 
-            answer.style.height = `${height}px`;
+            answer.style.height = `${height}px`; // si eso no fuera verdad que height es o se le regresa para que al momento de dar otro click se regrese el answer
         });
     });
 })();
--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 (function(){
     
-    const sliders = [...document.querySelectorAll('.testimony__body')];//agarramos en una variable sliders el id 
+    const sliders = [...document.querySelectorAll('.LenguajesPro__body')];//agarramos en una variable sliders el id , al agrgar los 3 puntos se combierte a array
     const buttonNext = document.querySelector('#next');//creo una variable para el boton siguiente 
     const buttonBefore = document.querySelector('#before');// boton atras
     let value;   
@@ -9,22 +9,22 @@
         changePosition(1);
     });
 
-    buttonBefore.addEventListener('click', ()=>{
+    buttonBefore.addEventListener('click', ()=>{//funcion click de atras que recorre el id de los carruseles
         changePosition(-1);
     });
 
-    const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
+    const changePosition = (add)=>{ // si presionamos los botones restara y sumara
+        const currentTestimony = document.querySelector('.LenguajesPro__body--show').dataset.id; //obtenemos el elemento con el data id pero solo body__show
+        value = Number(currentTestimony); //es para que al valor se le sume de acuerdo al boton seleccionado, si no se le agrega el  number los concatena
         value+= add;
 
 
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
+        sliders[Number(currentTestimony)-1].classList.remove('LenguajesPro__body--show');//es para remover el class
+        if(value === sliders.length+1 || value === 0){ //es para no revasar el nomero de sliders tanto como al inicio como al final
+            value = value === 0 ? sliders.length  : 1; //asi que si tenemos que sobrepasar el sliders regresara al  final de array o al inicio
         }
 
-        sliders[value-1].classList.add('testimony__body--show');
+        sliders[value-1].classList.add('LenguajesPro__body--show');//selleccionamos al elemento. value  y se le agrega la clase testomony__show
 
     }
 
